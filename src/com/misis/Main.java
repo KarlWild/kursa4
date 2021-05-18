d470b8b853644b2014406a6346bc91935678323f
KarlWild <39239382+KarlWild@users.noreply.github.com>
2021-05-19 00:06:16 +0300

    Initial commit

---
+++ src/com/misis/Main.java
@@ -0,0 +1,28 @@
+package com.misis;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("введите N");
+        int N = sc.nextInt();
+        System.out.println("Введите количество объектов M");
+        int M = sc.nextInt();
+        System.out.println("1. С повторениями\n2.без повторений");
+        int choice =sc.nextInt();
+        switch (choice){
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+    }
+}
